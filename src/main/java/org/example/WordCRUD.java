1312ee6e5ae1fc64852fa91c52b08be9b5c4bcaf
Eziwon <eziwon0228@gmail.com>
2023-09-07 00:27:32 +0900

    commit1

---
+++ src/main/java/org/example/WordCRUD.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class WordCRUD {
+    ArrayList<Word> list;
+    Scanner s;
+
+    WordCRUD(Scanner s) {
+        list = new ArrayList<>();
+        this.s = s;
+    }
+
+}
